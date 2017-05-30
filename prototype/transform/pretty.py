2fc1b3c9162e1b9d0770a677afcbe730e9f1313b
Markus Schöngart <markus+git@schoengart.net>
2017-05-31 00:18:05 +0200

    Added support for code transformation during tangling to transformer pretty

--- prototype/transform/pretty.py
+++ prototype/transform/pretty.py
@@ -3,20 +3,31 @@ from ..lib.toolchain import ContentTool
 
 
 class PrettyPrinter(ContentTool):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         super(PrettyPrinter, self).__init__()
+
+        self.weaving = False
+        if 'weave' in kwargs:
+            self.weaving = True
+
+        self.tangling = False
+        if 'tangle' in kwargs:
+            self.tangling = True
 
     def process_chunk(self, chunk, meta_data):
         yaweb = meta_data['yaweb']
 
-        if 'pretty_print' in yaweb.__dict__:
+        pretty_print = None
+
+        if self.weaving and 'pretty_print' in yaweb.__dict__:
             pretty_print = yaweb.__dict__['pretty_print']
-        else:
-            pretty_print = None
+
+        if self.tangling and 'pretty_print_code' in yaweb.__dict__:
+            pretty_print = yaweb.__dict__['pretty_print_code']
 
         if pretty_print and chunk.get('weave') == 'quoted':
             result = ast.clone(chunk)
-            pretty_print(result)
+            pretty_print(result, meta_data)
             return result
 
         else:
@@ -24,4 +35,4 @@ class PrettyPrinter(ContentTool):
 
 
 def pretty(*args, **kwargs):
-    return PrettyPrinter()
+    return PrettyPrinter(*args, **kwargs)
